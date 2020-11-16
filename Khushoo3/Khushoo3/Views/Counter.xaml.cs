8a1541929afedaa87ebc72c2d17124ce63f31041
Ahmed Ghaith <devghaith@outlook.com>
2020-11-16 03:12:18 +0200

    CounterAdded&QiblaUpdateed

---
+++ Khushoo3/Khushoo3/Views/Counter.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Khushoo3.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Counter : ContentView
+    {
+        int Num = 0;
+        int Numtens = 0;
+        int TotalNum = 0;
+
+        public Counter()
+        {
+            InitializeComponent();
+        }
+
+        private void CounterButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+               
+              
+               
+                TotalNum++;
+                Num++;
+
+                CounterButton.Text = Num.ToString();
+                if (Num == 33)
+                {
+                    Num = 0;
+                    Numtens++;
+                    var Sduration = TimeSpan.FromSeconds(0.5);
+                    Vibration.Vibrate(Sduration);
+
+                }
+
+                tens.Text = Numtens.ToString();
+                Total.Text = TotalNum.ToString();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+           
+        }
+    }
+}
--- Khushoo3/Khushoo3/Views/HomePage.xaml.cs
+++ Khushoo3/Khushoo3/Views/HomePage.xaml.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using Khushoo3.Models;
 using Khushoo3.ViewModel;
 using Newtonsoft.Json;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 
@@ -13,12 +14,19 @@ namespace Khushoo3.Views
 {
     public partial class HomePage : ContentPage
     {
+        double current_latitude = 0;
+        double current_longitude = 0;
+        double QiblaLatitude = 21.4224779;
+        double QiblaLongitude = 39.8251832;
+        double bearing_degree = 0;
+        SensorSpeed speed = SensorSpeed.UI;
         public HomePage()
         {
 
             InitializeComponent();
+            Compass.ReadingChanged += Compass_ReadingChanged;
+           
 
-          
         }
       
 
@@ -30,15 +38,13 @@ namespace Khushoo3.Views
 
         private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
         {
-            //    search.IsVisible = true;
-            //    Action<double> callback = input => SearchZekr.TranslationX = input;
-            //    SearchZekr.Animate("anim", callback, 260, 0, 16, 300, Easing.CubicInOut);
+            
             OpenMenu();
         }
         private void MenuTapped(object sender, EventArgs e)
         {
             OpenMenu();
-            // OpenVedio();
+           
 
         }
         private void OverlayTapped(object sender, EventArgs e)
@@ -66,17 +72,115 @@ namespace Khushoo3.Views
 
         }
 
-        private void QiblaTabbed(object sender, EventArgs e)
-        {
-            AzkarView.IsVisible = false;
-            QiblaView.IsVisible = true;
-        }
+      
 
         private void ZekrTabbed(object sender, EventArgs e)
         {
 
             AzkarView.IsVisible = true;
             QiblaView.IsVisible = false;
+            CounterView.IsVisible = false;
         }
+
+        private void CounterTapped(object sender, EventArgs e)
+        {
+            CounterView.IsVisible = true;
+            AzkarView.IsVisible = false;
+            QiblaView.IsVisible = false;
+        }
+
+
+
+
+        private void QiblaTabbed(object sender, EventArgs e)
+        {
+            QiblaIsAppear();
+            QiblaView.IsVisible = true;
+           
+            AzkarView.IsVisible = false;
+            CounterView.IsVisible = false;
+        }
+        private void QiblaIsAppear()
+        {
+           
+
+
+            if (!Compass.IsMonitoring) Compass.Start(speed);
+            GetLocation();
+
+          
+            PointToQibla();
+            if (!Compass.IsMonitoring) Compass.Start(speed);
+
+        }
+        bool isVibrate = false;
+        void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+        {
+            if (QiblaView.IsVisible)
+            {
+                int Value = Convert.ToInt32(e.Reading.HeadingMagneticNorth);
+
+                circularGauge.RotateTo(360 - e.Reading.HeadingMagneticNorth);
+                PointToQibla();
+
+                if (Value > bearing_degree - 2 && Value < bearing_degree + 2)
+                {
+
+                    pointer1.KnobColor = Color.Gold;
+                    scale.RimColor = Color.Gold;
+                    if (!isVibrate)
+                    {
+                        try
+                        {
+
+                            isVibrate = true;
+                            var duration = TimeSpan.FromSeconds(0.5);
+                            Vibration.Vibrate(duration);
+
+
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                    }
+                }
+                else
+                {
+                    isVibrate = false;
+                    pointer1.KnobColor = Color.White;
+                    circularGauge.BackgroundColor = Color.Transparent;
+                    scale.RimColor = Color.LightGray;
+
+                }
+            }
+        }
+
+
+        private async void GetLocation()
+        {
+            var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+            var location = await Geolocation.GetLocationAsync(request);
+            current_latitude = location.Latitude;
+            current_longitude = location.Longitude;
+        }
+
+        private double Mod(double a, double b)
+        {
+            return a - b * Math.Floor(a / b);
+        }
+
+        void PointToQibla()
+        {
+            double latt_from_radians = current_latitude * Math.PI / 180;
+            double long_from_radians = current_longitude * Math.PI / 180;
+            double latt_to_radians = QiblaLatitude * Math.PI / 180;
+            double lang_to_radians = QiblaLongitude * Math.PI / 180;
+            double bearing = Math.Atan2(Math.Sin(lang_to_radians - long_from_radians) * Math.Cos(latt_to_radians), (Math.Cos(latt_from_radians) * Math.Sin(latt_to_radians)) - (Math.Sin(latt_from_radians) * Math.Cos(latt_to_radians) * Math.Cos(lang_to_radians - long_from_radians)));
+            bearing = Mod(bearing, 2 * Math.PI);
+            bearing_degree = bearing * 180 / Math.PI;
+            pointer1.Value = bearing_degree;
+        }
+
     }
 }
